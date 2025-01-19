7d7cca59f96ab90a5a93b2e7f52faef07c90126a
levabala <levabala@ya.ru>
2025-01-19 08:31:17 +0700

    refactor: Simplify useQueryGetWord by removing unused fetchGetWord function

--- src/app/hooks/useQueryGetWord.ts
+++ src/app/hooks/useQueryGetWord.ts
@@ -1,20 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
-import { queryClient } from '../providers';
-import { WordData, WordStatus, DBWord } from '../types';
+import { WordData } from '../types';
 import { useQueryGetWordsAll } from './useQueryGetWordsAll';
-
-export async function fetchGetWord(textSource: string) {
-    const url = new URL('/api/words/one', window.location.origin);
-    url.searchParams.set('word', textSource);
-    
-    const res = await fetch(url).catch(() => null);
-    if (res === null) throw new Error();
-    
-    const json = await res.json();
-    if (!json?.word) throw new Error();
-    
-    return json as WordData;
-}
 
 export function useQueryGetWord(textSource: string) {
     const { data, isFetching } = useQueryGetWordsAll();
