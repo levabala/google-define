3d84987e69cfbd71eff55b69224a28f179a7d165
levabala <levabala@ya.ru>
2025-01-21 21:58:27 +0700

    chore: Remove page.spec.tsx and update matchers.d.ts with eslint rules

--- src/matchers.d.ts
+++ src/matchers.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
 import { Matchers, AsymmetricMatchers } from "bun:test";
 
