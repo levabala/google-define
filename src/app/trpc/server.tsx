ca3fc9e0a78514246e89da58c2f19f646d57be95
levabala <levabalaanother@gmail.com>
2025-03-17 21:16:06 +0100

    lol

--- src/app/trpc/server.tsx
+++ src/app/trpc/server.tsx
@@ -1,16 +1,17 @@
 import "server-only";
 
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
-import { cache } from "react";
-import { createTRPCContext } from "./init";
 import { makeQueryClient } from "./query-client";
 import { appRouter } from "./routers/_app";
+import { createTRPCContext } from "./init";
+import { cache } from "react";
 
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
 export const getQueryClient = cache(makeQueryClient);
 
 export const trpc = createTRPCOptionsProxy({
+    // @ts-expect-error -- i dunno
     ctx: createTRPCContext,
     router: appRouter,
     queryClient: getQueryClient,
