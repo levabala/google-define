d4f0afd880f618fc573324c63b42c3f78064c0fc
levabala <levabalaanother@gmail.com>
2025-07-25 23:09:04 +0300

    fix: viewport const export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,13 +29,14 @@ export const metadata: Metadata = {
     formatDetection: {
         telephone: false,
     },
-    viewport: {
-        width: "device-width",
-        initialScale: 1,
-        maximumScale: 1,
-        userScalable: false,
-        viewportFit: "cover",
-    },
+};
+
+export const viewport = {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+    viewportFit: "cover",
 };
 
 export default async function RootLayout({
